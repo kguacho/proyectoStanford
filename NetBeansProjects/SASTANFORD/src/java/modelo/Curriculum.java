6e9e3e7002f6274bb309f7cbff81be5cd61cba4b
KLEVER <kguacho2@gmail.com>
2015-09-03 16:47:23 -0500

    klever

---
+++ NetBeansProjects/SASTANFORD/src/java/modelo/Curriculum.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author david_000
+ */
+public class Curriculum {
+    private Integer codigo;
+    private Egresado egresado;
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public Egresado getEgresado() {
+        return egresado;
+    }
+
+    public void setEgresado(Egresado egresado) {
+        this.egresado = egresado;
+    }
+
+    public Curriculum(Integer codigo, Egresado egresado) {
+        this.codigo = codigo;
+        this.egresado = egresado;
+    }
+
+    public Curriculum() {
+    }
+    
+}
